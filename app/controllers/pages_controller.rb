46e8b3bb6fe830920a6273d43135ca8c3d38adcc
John Brown <jfbrown.wa@gmail.com>
2011-12-17 20:41:33 -0800

    post-transition commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 class PagesController < ApplicationController
 
   def home
-    if signed_in?
-      render :text => "You have successfully logged in as #{current_user.name}."
-    else
-      render :text => 'You have not logged in. By default, this template supports login via Facebook (/auth/facebook), Github (/auth/github), and Twitter (/auth/twitter [will not work from localhost]). Please try one of these to ensure that the system is working.'
-    end
   end
 
 end
