f626b7d16769f69a7ff5c1d4020d413f0bff6e9a
John Brown <jack@brownjohnf.com>
2012-01-24 10:58:25 +0000

    updated fb omniauth for pcsenegal.com on heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,7 +10,7 @@
 # for heroku
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '251660531567929', '6880cb188cecc364daa8f42d2810e47a'
+  provider :facebook, '217942814925150', 'a61046f5eb5f14c8571d51c404eef8e2'
   provider :twitter, 'w8kcCaZwobBBALc4inNo1g', 'nfCD1kIDXQ3eA72ys9aHZtnICMZgdOXtQ74HEgBg'
   provider :github, 'f5a3ee2fc018a0ed86f7', '3d085867f53f309a78902c90482be43f587aa0de'
   provider :google_oauth2, '85716079325.apps.googleusercontent.com', 'Wg9C0QLKD1FqNBJ-kT0NWrpW'
