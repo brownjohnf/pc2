a6c90f60051504b270ca0f302dd6b973e65092d8
John Brown <jack@brownjohnf.com>
2012-01-31 17:48:40 +0000

    tweaked splash page

---
+++ app/assets/javascripts/statics.js
@@ -0,0 +1,8 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+
+$(function() {
+  $('body.statics#splash .slideshow').css('background-image', 'url(/system/photos/1/large/4.JPG?1328024158)');
+});
