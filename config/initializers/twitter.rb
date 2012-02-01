dcbf50656098fad3a78b93568a328c90d204dedc
John Brown <jack@brownjohnf.com>
2012-02-01 13:52:00 +0000

    removed stupid twitter gem

--- config/initializers/twitter.rb
+++
@@ -1,7 +0,0 @@
-#Twitter::Base.new( '356760558-FUVfhspuzgVnBMk87KWjykjRb8c3eoNPRgio8YEH', 'rf4Any9eVWohCcRLxJclD2OIKl0SdwqtSmmP2rCssc' )
-Twitter.configure do |config|
-  config.consumer_key = 'Sezd3JOYG6pl9xsI7W9FAA'
-  config.consumer_secret = '5GicL6kizE4EN3Gwb5Ev5br14dFXaNlgYKvAsSrjs'
-  config.oauth_token = '356760558-FUVfhspuzgVnBMk87KWjykjRb8c3eoNPRgio8YEH'
-  config.oauth_token_secret = 'rf4Any9eVWohCcRLxJclD2OIKl0SdwqtSmmP2rCssc'
-end
