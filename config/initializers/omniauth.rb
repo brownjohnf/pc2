f2a4963e3d06885be12ee5afc28da37b4437c8e7
John Brown <jack@brownjohnf.com>
2012-02-08 12:40:11 +0000

    added twitter support

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # for heroku
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '217942814925150', 'a61046f5eb5f14c8571d51c404eef8e2'
-  provider :twitter, 'XhPJ732UpRSSawGZVAh3oQ', 'S4FIZTRCpDWsJNFzmEgdfIa5upRBo74yA28tZbxTo'
+  provider :twitter, 'uESiC9KsmBPPqdmlMEmQzQ', 'hhA4HLlk3q1Dqe2RdiaFOjAa3xFc32qhmdmQpEg'
   provider :github, '', ''
   provider :google_oauth2, '', ''
   provider :identity, on_failed_registration: lambda { |env|
