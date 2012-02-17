451fa5b52668642b74a3aa5a4a31da34324c9361
John Brown <jack@brownjohnf.com>
2012-02-17 11:58:38 +0000

    fixed country error on signup

--- config/routes.rb
+++ config/routes.rb
@@ -121,6 +121,8 @@ OmniauthDemo::Application.routes.draw do
 
   match '/feedback', :to => 'feedback#new'
   match '/feed/pages', :to => 'pages#updated'
+  
+  match '/facebook' => redirect('https://apps.facebook.com/peacecorps_sn/')
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
