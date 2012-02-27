ed4eb3046ec3e40b83ced4697e2be7e0d0876f17
John Brown <jack@brownjohnf.com>
2012-02-27 11:29:15 +0000

    added titles to user menu, fixed empty doc icon

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -4,9 +4,10 @@ module DocumentsHelper
     sizes = { :icon => '80', :thumb => 100, :small => '200', :medium => '380', :large => '980', :full => '1140' }
     icons = {
       'application/pdf' => 'text/filetype_pdf.png',
-      'audio/mp3' => 'audio/filetype_mp3.png'
+      'audio/mp3' => 'audio/filetype_mp3.png',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'text/filetype_doc.png'
     }
-    link_to image_tag("icons/filetypes/#{icons[document.file_content_type]}", :width => sizes[options[:size]], :style => 'box-shadow: none;'), document, :class => 'avatar'
+    link_to image_tag("icons/filetypes/#{icons[document.file_content_type] ? icons[document.file_content_type] : 'extras/filetype_blank.png'}", :width => sizes[options[:size]], :style => 'box-shadow: none;'), document, :class => 'avatar'
   end
 
 end
