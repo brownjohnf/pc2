798c0cd7b6508d5ec78f200e482a4ae75b281114
John Brown <jack@brownjohnf.com>
2012-03-15 18:42:15 +0000

    added stick subnav

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -31,6 +31,26 @@ $(function() {
 });
 */
 $(function(){
+	// fix sub nav on scroll
+    var $win = $(window)
+      , $nav = $('.subnav')
+      , navTop = $('.subnav').length && $('.subnav').offset().top - 40
+      , isFixed = 0
+
+    processScroll()
+
+    $win.on('scroll', processScroll)
+
+    function processScroll() {
+      var i, scrollTop = $win.scrollTop()
+      if (scrollTop >= navTop && !isFixed) {
+        isFixed = 1
+        $nav.addClass('subnav-fixed')
+      } else if (scrollTop <= navTop && isFixed) {
+        isFixed = 0
+        $nav.removeClass('subnav-fixed')
+      }
+    }
 	$('.box.longform p img').attr('rel', 'group').colorbox({
 		title: 'Test title',
 		href: function(){ return $(this).attr('src') + '.jpg'; },
