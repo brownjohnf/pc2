9d0edb4b43ba1cc338f97f2d1168c2e80bbdbb8f
John Brown <jack@brownjohnf.com>
2012-04-09 20:12:34 +0000

    setting up security for moments

--- spec/controllers/moments_controller_spec.rb
+++ spec/controllers/moments_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe UsersController do
+describe MomentsController do
 
   render_views
 
@@ -10,8 +10,43 @@ describe UsersController do
       response.should be_success
     end
     it 'should respond to JSON request' do
-      get :index, :format => 'json'
+      get :index, :all => true, :format => 'json'
       response.should be_success
+    end
+  end
+
+  describe "'GET' edit" do
+    context 'as anyone' do
+      before(:each) do
+        @moment = Factory.create(:moment)
+      end
+      it 'should fail' do
+        get :edit, :id => @moment
+        response.should_not be_success
+      end
+    end
+    context 'as user' do
+      login_user
+      describe 'who is owner' do
+        before(:each) do
+          @moment = Factory.create(:moment, :user => @user)
+        end
+        it 'should be successful' do
+          get :edit, :id => @moment
+          puts response.body.inspect
+          response.should be_success
+        end
+      end
+    end
+    context 'as admin' do
+      before(:each) do
+        @moment = Factory.create(:moment)
+      end
+      login_admin
+      it 'should be successful' do
+        get :edit, :id => @moment
+        response.should be_success
+      end
     end
   end
 end
