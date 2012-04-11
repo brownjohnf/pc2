27d4c9f78585b2093eab429f17834c7e6341457c
John Brown <jack@brownjohnf.com>
2012-04-11 15:56:00 +0000

    refinement of timeline capabilities

--- app/models/moment.rb
+++ app/models/moment.rb
@@ -1,4 +1,5 @@
 class Moment < ActiveRecord::Base
+  include ActionView::Helpers::TextHelper
 
   acts_as_taggable_on :tags
 
@@ -22,6 +23,36 @@ class Moment < ActiveRecord::Base
     if (c = count) != 0
       find(:first, :offset =>rand(c))
     end
+  end
+
+  def text
+    if photo && content
+      ActiveSupport::JSON.encode('<p>' + summary + '</p><p><div style="display:inline-block;float:left;">' + user.name + '</div>Submitted by ' + user.name + '<br />' + Carmen.country_name(country) + '</p><p>' + truncate(content, :length => 150, :separator => ' ') + '</p>')
+    elsif content
+      ActiveSupport::JSON.encode('<p>' + summary + '</p><p><div style="display:inline-block;float:left;">' + user.name + '</div>Submitted by ' + user.name + '<br />' + Carmen.country_name(country) + '</p>')
+    else
+      ActiveSupport::JSON.encode('<p><div style="display:inline-block;float:left;">' + user.name + '</div>Submitted by ' + user.name + '<br />' + Carmen.country_name(country) + '</p>')
+    end
+  end
+
+  def media
+    if photo && content
+      ActiveSupport::JSON.encode(photo.photo.url)
+    elsif content && content.length < 100
+      ActiveSupport::JSON.encode(content)
+    elsif content
+      ActiveSupport::JSON.encode('<blockquote>' + truncate(content, :length => 500, :separator => ' ') + '</blockquote>')
+    else
+      ActiveSupport::JSON.encode('<blockquote>' + summary + '</blockquote>')
+    end
+  end
+
+  def credit
+    ActiveSupport::JSON.encode(photo ? photo.user.name : user.name)
+  end
+
+  def caption
+    ActiveSupport::JSON.encode((photo && photo.description) ? photo.description : summary)
   end
 
   private
