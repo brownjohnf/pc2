64966a2b6130506d184f5f81453e3a2a5bc88e27
John Brown <jack@brownjohnf.com>
2012-04-14 02:32:10 +0000

    changed model to use s3

--- app/models/document.rb
+++ app/models/document.rb
@@ -31,8 +31,12 @@ class Document < ActiveRecord::Base
 
   # source file for document, for archiving
   has_attached_file :source, {
-    :path => "public/system/#{Rails.env}/:attachment/:id/:style/:filename",
-    :url => "/system/#{Rails.env}/:attachment/:id/:style/:filename"
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET'],
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }
   }
   validates_attachment_content_type :source, :content_type => [
     'text/plain', 
