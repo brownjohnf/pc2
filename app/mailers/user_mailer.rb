9e2312f04657a4aec61202457bfd2c69eb6a6480
John F. Brown <jack@brownjohnf.com>
2012-07-18 18:18:14 +0000

    cleaned up moment form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,6 @@
 class UserMailer < ActionMailer::Base
  
   def signup_notification(user)
-    recipients "#{user.name} <#{user.email}"
-    from 'Peace Corps Senegal'
-    subject 'Congrats!'
-    sent_on Time.now
-    body {
-      :user => user,
-      :url => "http://www.pcsenegal.org"
-    }
   end
 
 end
